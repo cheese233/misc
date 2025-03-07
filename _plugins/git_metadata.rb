c05c66392f87686d618dcc2937cf76417b24f6d1
cheese233 <cheese-233@outlook.com>
2025-03-08 00:50:36 +0800

    2

--- _plugins/git_metadata.rb
+++ _plugins/git_metadata.rb
@@ -1,7 +1,7 @@
 Jekyll::Hooks.register :documents, :post_init do |doc|
     git_dates_log_command = `git log --follow --format=%ad --date=iso-strict -- "#{doc.path}"`
     git_dates = git_dates_log_command.split("\n")
-    doc.data["last_modified_at"] ||= git_dates.first
+    doc.data["last_modified_date"] ||= git_dates.first
     doc.data["date"] ||= git_dates.last
     git_log_command = `git log --follow --format='%s' -- "#{doc.path}"`
     commit_messages = git_log_command.split("\n")
